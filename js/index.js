9bcce483bbf3bfa324f8f77389e4aa1a1670740f
Andrey Kozlov <andrew08552gmail.com>
2023-04-03 01:17:52 +0300

    simplified the code

--- js/index.js
+++ js/index.js
@@ -2,27 +2,27 @@
 
 let modal        = document.querySelector(".modal"),
     btn          = document.querySelectorAll(".card__btn_open"),
-    btn_del_card = document.querySelector(".header__logIn");
+    btn_del_card = document.querySelectorAll(".card__btn_del");
 
 document.querySelector(".modal__close").addEventListener("click",() => {
-    modal.style.visibility = 'hidden';
-    modal.style.opacity    = "0";
+    modal.classList.toggle("active")
 })
 
 btn.forEach(item => {
     item.addEventListener("click",function(link){
-        console.log(1)
-        // get_card_details(link)
-        // link.target.parentElement.remove()
-        modal.style.visibility = 'visible';
-        modal.style.opacity    = "1";
+        modal.classList.toggle("active")
         get_card_details(link)
     })
 })
+
+btn_del_card.forEach(item => {
+    item.addEventListener("click",function(link){
+        link.target.parentElement.parentElement.classList.add('animate');
+        setTimeout(() => link.target.parentElement.parentElement.remove(), 1000);
+    })
+})
+
 function get_card_details(link){
     modal.querySelector(".modal__img-item").src    = link.target.parentElement.parentElement.querySelector(".card__image-item").src
     modal.querySelector(".modal__title").innerText = link.target.parentElement.parentElement.querySelector(".card__title").innerText
 }
-// btn_del_card.addEventListener("click", function(e){
-//     console.log(e.target)
-// })
