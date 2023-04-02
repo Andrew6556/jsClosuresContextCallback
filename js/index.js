72233ae45849b5ee54ea6c2bd082cefe315bec98
Andrey Kozlov <andrew08552gmail.com>
2023-04-02 16:00:32 +0300

    done receiving data from the card

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
-// "use strict";
+"use strict";
 
-function myFunction() {
-    console.log(this);
-}
+let modal = document.querySelector(".modal"),
+    btn   = document.querySelectorAll(".card__btn");
 
-// Вызовем функцию
-myFunction();
+btn.forEach(item => {
+    item.addEventListener("click",function(link){
+        
 
-// console.log(this)
+        modal.querySelector(".modal__img-item").src = link.target.parentElement.querySelector(".card__image-item").src
+        modal.querySelector(".modal__title").innerText = link.target.parentElement.querySelector(".card__title").innerText
+        console.log(link.target.parentElement.querySelector(".card__image-item").src)
+        modal.style.visibility = 'visible';
+        modal.style.opacity   = "1";
+    })
+})
