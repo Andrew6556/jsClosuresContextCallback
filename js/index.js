34d70e458222ea3364b89c4f81b4001d17cbf6d4
Andrey Kozlov <andrew08552gmail.com>
2023-04-03 00:17:53 +0300

    changed button design

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let modal        = document.querySelector(".modal"),
-    btn          = document.querySelectorAll(".card__btn"),
+    btn          = document.querySelectorAll(".card__btn_open"),
     btn_del_card = document.querySelector(".header__logIn");
 
 document.querySelector(".modal__close").addEventListener("click",() => {
@@ -11,17 +11,17 @@ document.querySelector(".modal__close").addEventListener("click",() => {
 
 btn.forEach(item => {
     item.addEventListener("click",function(link){
-        get_card_details(link)
+        console.log(1)
+        // get_card_details(link)
         // link.target.parentElement.remove()
-        // console.log(link.target.parentElement.remove())
         modal.style.visibility = 'visible';
         modal.style.opacity    = "1";
         get_card_details(link)
     })
 })
 function get_card_details(link){
-    modal.querySelector(".modal__img-item").src    = link.target.parentElement.querySelector(".card__image-item").src
-    modal.querySelector(".modal__title").innerText = link.target.parentElement.querySelector(".card__title").innerText
+    modal.querySelector(".modal__img-item").src    = link.target.parentElement.parentElement.querySelector(".card__image-item").src
+    modal.querySelector(".modal__title").innerText = link.target.parentElement.parentElement.querySelector(".card__title").innerText
 }
 // btn_del_card.addEventListener("click", function(e){
 //     console.log(e.target)
