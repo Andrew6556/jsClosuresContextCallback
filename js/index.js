55a94fdb32a0ec245ae2e7b9fbb28b64e9c88ad9
Andrey Kozlov <andrew08552gmail.com>
2023-04-03 01:45:48 +0300

    proceeded to data form

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 "use strict";
 
-let modal        = document.querySelector(".modal"),
+let modal        = document.querySelector(".modalInfo"),
     btn          = document.querySelectorAll(".card__btn_open"),
     btn_del_card = document.querySelectorAll(".card__btn_del");
 
 document.querySelector(".modal__close").addEventListener("click",() => {
     modal.classList.toggle("active")
 })
-
+document.querySelector(".header__form").addEventListener("click", ()=>{
+    console.log(1)
+    document.querySelector(".modalForm").classList.toggle("active")
+})
 btn.forEach(item => {
     item.addEventListener("click",function(link){
         modal.classList.toggle("active")
@@ -23,6 +26,6 @@ btn_del_card.forEach(item => {
 })
 
 function get_card_details(link){
-    modal.querySelector(".modal__img-item").src    = link.target.parentElement.parentElement.querySelector(".card__image-item").src
-    modal.querySelector(".modal__title").innerText = link.target.parentElement.parentElement.querySelector(".card__title").innerText
+    modal.querySelector(".modal__img-item").src    = link.target.parentElement.parentElement.querySelector(".card__image-item").src;
+    modal.querySelector(".modal__title").innerText = link.target.parentElement.parentElement.querySelector(".card__title").innerText;
 }
