94d72675538ab31265311acd0c5636cc7222c16d
Andrey Kozlov <andrew08552gmail.com>
2023-04-05 06:02:26 +0300

    improve js code

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
 "use strict";
 
-let modal        = document.querySelector(".modalInfo"),
-    btn          = document.querySelectorAll(".card__btn_open"),
-    btn_del_card = document.querySelectorAll(".card__btn_del");
+let modal         = document.querySelectorAll(".modal"),
+    modal_close   = document.querySelectorAll(".modal__close"),
+    btn_open_card = document.querySelectorAll(".card__btn_open"),
+    btn_del_card  = document.querySelectorAll(".card__btn_del");
 
-document.querySelector(".modal__close").addEventListener("click",() => {
-    modal.classList.toggle("active")
+modal_close.forEach(item_close=>{
+    item_close.addEventListener("click", link =>{
+        // Удаление класса внезависмости от формы
+        link.target.parentElement.parentElement.parentElement.classList.toggle("active")
+        // Мне вот это самому не нравится но лучше способа не нашел:(
+    })
 })
+
 document.querySelector(".header__form").addEventListener("click", ()=>{
-    console.log(1)
-    document.querySelector(".modalForm").classList.toggle("active")
+    // появление формы
+    modal[1].classList.toggle("active")
 })
-btn.forEach(item => {
+btn_open_card.forEach(item => {
     item.addEventListener("click",function(link){
-        modal.classList.toggle("active")
+        modal[0].classList.toggle("active")
+        console.log(modal)
         get_card_details(link)
     })
 })
@@ -26,6 +33,6 @@ btn_del_card.forEach(item => {
 })
 
 function get_card_details(link){
-    modal.querySelector(".modal__img-item").src    = link.target.parentElement.parentElement.querySelector(".card__image-item").src;
-    modal.querySelector(".modal__title").innerText = link.target.parentElement.parentElement.querySelector(".card__title").innerText;
+    modal[0].querySelector(".modal__img-item").src    = link.target.parentElement.parentElement.querySelector(".card__image-item").src;
+    modal[0].querySelector(".modal__title").innerText = link.target.parentElement.parentElement.querySelector(".card__title").innerText;
 }
